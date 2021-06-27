009261bafddaecf560ff6bcf4f37f28a62b86240
HaiYing Wang <whygps@gmail.com>
2021-06-27 09:22:07 -0400

    add evil and evil-tutor

--- init.el
+++ init.el
@@ -97,7 +97,7 @@
  '(markdown-command "/usr/bin/pandoc")
  '(org-agenda-files '("~/Dropbox/mydoc/web/web.org"))
  '(package-selected-packages
-   '(forge auctex auto-highlight-symbol beacon browse-kill-ring color-theme-sanityinc-tomorrow gnu-elpa-keyring-update company-math edit-server edit-server-htmlize eglot eglot-jl ess highlight-indentation highlight-numbers highlight-parentheses julia-mode julia-repl kill-ring-search lsp-mode lsp-julia magit org pdf-tools pdf-view-restore polymode poly-R poly-markdown use-package vterm which-key)))
+   '(evil-tutor evil forge auctex auto-highlight-symbol beacon browse-kill-ring color-theme-sanityinc-tomorrow gnu-elpa-keyring-update company-math edit-server edit-server-htmlize eglot eglot-jl ess highlight-indentation highlight-numbers highlight-parentheses julia-mode julia-repl kill-ring-search lsp-mode lsp-julia magit org pdf-tools pdf-view-restore polymode poly-R poly-markdown use-package vterm which-key)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -124,6 +124,10 @@
 (require 'polymode-config)
 (require 'term-config)
 (require 'theme-highlight-config)
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
 
 (provide 'init)
 ;;; init ends here
