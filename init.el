b2768fe14aaf8d87c87e057941cb3010a016c1cf
HaiYing Wang <whygps@gmail.com>
2021-06-27 18:37:29 -0400

    Add undo-tree for C-r

--- init.el
+++ init.el
@@ -97,7 +97,7 @@
  '(markdown-command "/usr/bin/pandoc")
  '(org-agenda-files '("~/Dropbox/mydoc/web/web.org"))
  '(package-selected-packages
-   '(evil-tutor evil forge auctex auto-highlight-symbol beacon browse-kill-ring color-theme-sanityinc-tomorrow gnu-elpa-keyring-update company-math edit-server edit-server-htmlize eglot eglot-jl ess highlight-indentation highlight-numbers highlight-parentheses julia-mode julia-repl kill-ring-search lsp-mode lsp-julia magit org pdf-tools pdf-view-restore polymode poly-R poly-markdown use-package vterm which-key)))
+   '(undo-tree evil-tutor evil forge auctex auto-highlight-symbol beacon browse-kill-ring color-theme-sanityinc-tomorrow gnu-elpa-keyring-update company-math edit-server edit-server-htmlize eglot eglot-jl ess highlight-indentation highlight-numbers highlight-parentheses julia-mode julia-repl kill-ring-search lsp-mode lsp-julia magit org pdf-tools pdf-view-restore polymode poly-R poly-markdown use-package vterm which-key)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -128,6 +128,8 @@
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
+(global-undo-tree-mode) ;; need undo tree before Emacs 28
+(evil-set-undo-system 'undo-tree)
 
 (provide 'init)
 ;;; init ends here
