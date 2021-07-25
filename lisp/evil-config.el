7bb8a01eb5835e293f240d0039e7437448c5b3c3
HaiYing Wang <whygps@gmail.com>
2021-07-24 23:00:00 -0400

    get back some back basic emacs commands

--- lisp/evil-config.el
+++ lisp/evil-config.el
@@ -35,6 +35,16 @@
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
+;;; basic emacs commands
+(define-key evil-normal-state-map "\C-y" 'yank)
+(define-key evil-insert-state-map "\C-y" 'yank)
+(define-key evil-visual-state-map "\C-y" 'yank)
+(define-key evil-insert-state-map "\C-e" 'end-of-line)
+(define-key evil-normal-state-map "\C-w" 'evil-delete)
+(define-key evil-insert-state-map "\C-w" 'evil-delete)
+(define-key evil-insert-state-map "\C-r" 'search-backward)
+(define-key evil-visual-state-map "\C-w" 'evil-delete)
+
 (provide 'evil-config)
 
 ;; ;; Enable Evil
