974b7f9a8e897c79e6b2c8ac000cfdc7a8bb324e
HaiYing Wang <whygps@gmail.com>
2021-10-22 23:45:42 -0400

    change mode-line-inactive color

--- lisp/theme-highlight-config.el
+++ lisp/theme-highlight-config.el
@@ -17,7 +17,8 @@
 (set-face-background 'tool-bar "lightgoldenrod2")
 (set-face-foreground 'font-lock-builtin-face "Cyan")
 (set-face-foreground 'minibuffer-prompt "GreenYellow")
-(set-face-background 'mode-line-inactive "goldenrod")
+;; (set-face-background 'mode-line-inactive "goldenrod")
+(set-face-background 'mode-line-inactive "gray11")
 
 (add-to-list 'default-frame-alist '(foreground-color . "White"))
 (add-to-list 'default-frame-alist '(background-color . "Black"))
