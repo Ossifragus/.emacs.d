ab552b63c7b2a95da175031bcdc3a9eb1ee88cce
HaiYing Wang <whygps@gmail.com>
2021-11-20 20:52:45 -0500

    move term face config to term-config.el

--- lisp/term-config.el
+++ lisp/term-config.el
@@ -106,4 +106,20 @@
     )
   (add-hook 'vterm-mode-hook #'set-vterm-keys))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(vterm-color-black ((t (:foreground "#3F3F3F" :background "#992323"))))
+ '(vterm-color-blue ((t (:foreground "#7CB8BB" :background "#4C7073"))))
+ '(vterm-color-cyan ((t (:foreground "#93E0E3" :background "#8CD0D3"))))
+ '(vterm-color-green ((t (:foreground "#7F9F7F" :background "#9FC59F"))))
+ '(vterm-color-magenta ((t (:foreground "#DC8CC3" :background "#CC9393"))))
+ '(vterm-color-red ((t (:foreground "#AC7373" :background "#8C5353"))))
+ '(vterm-color-white ((t (:foreground "#DCDCCC" :background "#656555"))))
+ '(vterm-color-yellow ((t (:foreground "#DFAF8F" :background "#9FC59F"))))
+ '(vterm-default-bg-color ((t (:inherit vterm-color-black))))
+ '(vterm-default-fg-color ((t (:inherit vterm-color-white)))))
+
 (provide 'term-config)
