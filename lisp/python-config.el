e8df88571b800e16eab62749830676037d5e6eec
HaiYing Wang <whygps@gmail.com>
2022-05-05 11:43:20 -0400

    add elpy

---
+++ lisp/python-config.el
@@ -0,0 +1,12 @@
+;; https://github.com/jorgenschaefer/elpy/issues/1729
+;; M-x elpy-rpc-reinstall-virtualenv
+(use-package elpy
+  :ensure t
+  :defer t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable))
+
+;; https://emacs.stackexchange.com/questions/47358/can-t-guess-python-indent-offset-delete-warning-when-local-file-variable
+(setq python-indent-guess-indent-offset-verbose nil)
+
+(provide 'python-config)
