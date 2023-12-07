a9f7e43f8bd3ea5f0c7c2fee0d15442913d76e7b
HaiYing Wang <whygps@gmail.com>
2023-12-06 22:31:45 -0500

    some cleaning

--- init.el
+++ init.el
@@ -1,16 +1,13 @@
 ;; different settings for different computers
 (cond
- ((string-equal (system-name) "T1700")
+ ((or (string= (system-name) "aust319")
+      (string= (system-name) "T1700")
+      (string= (system-name) "W319"))
   (progn
     (setq default-frame-alist
 	  '((top . 0) (left . 72) (height . 56) (width . 81)))
     ))
- ((or (string-equal (system-name) "aust319") (string-equal (system-name) "W319"))
-  (progn
-    (setq default-frame-alist
-	  '((top . 0) (left . 72) (height . 56) (width . 81)))
-    ))
- ((or (eq system-type 'darwin) (string-equal (system-name) "MBP16.local"))
+ ((or (eq system-type 'darwin) (string= (system-name) "MBP16.local"))
   (progn
     (setq default-frame-alist
 	  '((top . 0) (left . 0) (height . 100) (width . 81)))
