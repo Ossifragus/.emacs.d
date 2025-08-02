656216354754e3dc084f2f8d5dd007ef3323b971
HaiYing Wang <whygps@gmail.com>
2025-08-01 22:02:39 -0400

    open word files in libreoffice

--- lisp/system-config.el
+++ lisp/system-config.el
@@ -3,8 +3,12 @@
   :config
   (require 'openwith)
   (setq openwith-associations '(("\\.pdf\\'" "evince" (file))))
+  (setq openwith-associations '(("\\.odt\\'" "libreoffice" (file))))
+  (setq openwith-associations '(("\\.doc\\'" "libreoffice" (file))))
+  (setq openwith-associations '(("\\.docx\\'" "libreoffice" (file))))
   (openwith-mode t)
   )
+;; (rassq-delete-all #'doc-view-mode-maybe auto-mode-alist)
 
 (use-package exec-path-from-shell
   :ensure t
