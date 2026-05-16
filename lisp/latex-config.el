c415d3f3e5fdd30290314ca74e39c2b2e9670410
HaiYing Wang <whygps@gmail.com>
2026-05-15 23:29:41 -0400

    fix inverse search focus on Wayland/GNOME

--- lisp/latex-config.el
+++ lisp/latex-config.el
@@ -59,14 +59,6 @@
 
 (add-hook 'LaTeX-mode-hook 'my-TeX-evince-register-inverse-search)
 
-;; Alternative 3: Use the external wmctrl tool in order to
-;; force Emacs into the focus.
-(setq TeX-raise-frame-function
-      (lambda ()
-	(call-process
-	 "wmctrl" nil nil nil "-i" "-R"
-	 (frame-parameter (selected-frame) 'outer-window-id))))
-
 (defun my-LaTeX-hook ()
 	(set-face-foreground 'font-latex-math-face "burlywood")
 	(set-face-foreground 'font-latex-warning-face "red")
@@ -114,94 +106,41 @@
                         (kbd "<leader>cc") 'TeX-command-master)
             ))
 
-(cond
- ((or (eq system-type 'darwin) (string= (system-name) "MBP16.local"))
-  (progn
+(with-eval-after-load 'tex
+  (cond
+   ((or (eq system-type 'darwin) (string= (system-name) "MBP16.local"))
     (setq TeX-source-correlate-method 'synctex
-          TeX-view-program-list   ;; Use Skim, it's awesome
+          TeX-view-program-list
           '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline -g -b %n %o %b"))
-          TeX-view-program-selection '((output-pdf "Skim")))
-    ;; (setq mac-option-key-is-meta nil)
-    ))
- ((eq system-type 'gnu/linux)
-  (add-to-list 'TeX-view-program-list
-               '("Evince-Pascal" my-TeX-evince-sync-view))
-  (setq TeX-view-program-selection '((output-pdf "Evince-Pascal")))))
+          TeX-view-program-selection '((output-pdf "Skim"))))
+   ((eq system-type 'gnu/linux)
+    (add-to-list 'TeX-view-program-list
+                 '("Evince-Pascal" my-TeX-evince-sync-view))
+    (setq TeX-view-program-selection '((output-pdf "Evince-Pascal")))))
+
+  ;; Focus strategy for PGTK/Wayland/GNOME
+  (setq TeX-raise-frame-function
+        (lambda ()
+          (let ((frame (selected-frame)))
+            (run-at-time 0.1 nil
+                         (lambda (f)
+                           (when (frame-live-p f)
+                             (make-frame-visible f)
+                             (select-frame-set-input-focus f)
+                             (raise-frame f)
+                             (if (fboundp 'x-focus-frame) (x-focus-frame f))
+                             (if (fboundp 'pgtk-focus-frame) (pgtk-focus-frame f))
+                             ;; Fallback for GNOME
+                             (when (executable-find "wmctrl")
+                               (call-process "wmctrl" nil nil nil "-a" "Emacs"))))
+                         frame)))))
 
 (provide 'latex-config)
 
-;; ;; Alternative 1: For some users, `x-focus-frame' works.
-;; (setq TeX-raise-frame-function #'x-focus-frame)
-
-;; ;; Alternative 2: Under GNOME 3.20 (and probably others), it
-;; ;; seems some focus stealing prevention policy prohibits that
-;; ;; some window gets the focus immediately after the user has
-;; ;; clicked in some other window.  Here waiting a bit before
-;; ;; issuing the request seems to work.
-;; (setq TeX-raise-frame-function
-;;       (lambda ()
-;; 	(run-at-time 0.5 nil #'x-focus-frame)))
-
-;; (require 'flymake)
-;; (defun flymake-get-tex-args (file-name)
-;; (list "pdflatex"
-;; (list "-file-line-error" "-draftmode" "-interaction=nonstopmode" file-name)))
-
-
-;; ;; using auctex
-;; (use-package tex
-;;   :ensure auctex)
-
-;; ;; Turn on RefTeX in AUCTeX
-;; (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-
-;; ;; Activate nice interface between RefTeX and AUCTeX
-;; (setq reftex-plug-into-AUCTeX t)
-
-;; ;;; add company backend for latex math
-;; (use-package company-math
-;;   :ensure t)
-;; (eval-after-load 'company
-;;   '(add-to-list 'company-backends 'company-math-symbols-unicode))
-
-;; ;;; using ivy-bibtex
-;; (use-package ivy-bibtex
-;;   :ensure t)
-;; (global-set-key (kbd "M-s b") 'ivy-bibtex)
-
-;; (setq bibtex-completion-bibliography
-;;       '("~/bibrary/bib/index.bib"))
-;; (setq bibtex-completion-library-path
-;;       '("~/bibrary/papers"
-;;         "~/bibrary/books"
-;;         "~/bibrary/misc"))
-
-;; ;;; I do not use notes anyway
-;; ;; (setq bibtex-completion-notes-path
-;; ;;       "~/bibrary/notes.org")
-;; (setq bibtex-completion-notes-path nil)
-;; (setq bibtex-completion-pdf-field nil)
-;; (setq bibtex-completion-find-additional-pdfs t)
-;; (setq bibtex-completion-pdf-extension '(".pdf" ".djvu"))
-;; (unless *is-a-mac*
-;;   (setq bibtex-completion-pdf-open-function
-;;         (lambda (fpath)
-;;           (call-process "okular" nil 0 nil fpath))))
-
-;; ;;; using gsholar-bibtex
-;; (use-package gscholar-bibtex
-;;   :ensure t)
-
-;; ;;; auto run bib-validate-globally whenever saving bibtex files
-;; (defun bibtex-auto-validate-globally ()
-;;   "Run bibtex-validate-globally after saving the bibtex files."
-;;   (when (eq major-mode 'bibtex-mode)
-;;     (bibtex-validate-globally)))
-;; (add-hook 'after-save-hook #'bibtex-auto-validate-globally)
-
-;; (setenv "PATH"
-;; 				(concat
-;; 				 "/usr/local/texlive/2019/bin/x86_64-linux" ":"
-;; 				 (getenv "PATH")))
-
-;; (setq exec-path (append '("/usr/local/texlive/2019/bin/x86_64-linux") exec-path))
+;; for inverse search to bring emacs to front
+;; This is the most comprehensive customization tool for GNOME and is usually the first to be updated for new GNOME versions.
+;; 1.  Search for Just Perfection in your Extension Manager or on the GNOME Extensions website.
+;; 2.  Install and open its Settings.
+;; 3.  Go to the Behavior tab.
+;; 4.  Look for Window Demands Attention Focus and turn it ON.
+;; 5.  This will bypass the "Window is ready" notification and focus Emacs immediately.
