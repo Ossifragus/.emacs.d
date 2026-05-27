4ff6098ec3a06106dd490a2026889ab9d530e3e7
HaiYing Wang <whygps@gmail.com>
2026-05-26 22:25:59 -0400

    antigravity cli as the backend for ai-code-interface.el

--- lisp/chatgpt-config.el
+++ lisp/chatgpt-config.el
@@ -117,7 +117,8 @@
   ;; :straight (:host github :repo "tninja/ai-code-interface.el") ;; if you want to use straight to install, no need to have MELPA setting above
   :config
   ;; use codex as backend, other options are 'claude-code, 'gemini, 'github-copilot-cli, 'opencode, 'grok, 'cursor, 'kiro, 'codebuddy, 'aider, 'agent-shell, 'claude-code-ide, 'claude-code-el
-  (ai-code-set-backend 'agent-shell)
+  ;; (ai-code-set-backend 'agent-shell)
+  (ai-code-set-backend 'antigravity)
   ;; Enable global keybinding for the main menu
   ;; (global-set-key (kbd "C-c a") #'ai-code-menu)
   (bind-key* (kbd "C-c a") #'ai-code-menu); The * creates a super-global binding
